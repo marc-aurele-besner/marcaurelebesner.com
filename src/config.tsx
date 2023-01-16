cc79983f0ed1d773b623b4fec4d1743dbf43cd28
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-01-15 19:49:00 -0500

    update dependencies

--- src/config.tsx
+++ src/config.tsx
@@ -5,9 +5,7 @@ const url = 'https://react-three-next-ts.netlify.app/'
 const description = 'The easiest and fastest way to create a 3D website using React Three Fiber and NextJS'
 const author = 'Author'
 
-const Header = ({
-  title = titleDefault
-}) => {
+const Header = ({ title = titleDefault }) => {
   return (
     <Head>
       {/* Recommended Meta Tags */}
